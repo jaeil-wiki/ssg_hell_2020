d7594404cb25ef5b2ef4a6448b2a2ff8477b2313
Jaeil Lee <dev@jaeil.wiki>
2020-02-16 16:28:22 +0900

    implement sample test.

--- Sample/tests.py
+++ Sample/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
-
 # Create your tests here.
+from unittest import TestCase
+
+
+class SampleTest(TestCase):
+    def test_something(self):
+        expected = 1
+        actual = 2
+
+        self.assertEqual(expected, actual)
